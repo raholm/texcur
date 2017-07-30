41728cf46f253f1f938b88ceed0661652bf3b330
raholm <rasmus.holm723@gmail.com>
2017-07-31 00:26:33 +0200

    Implemented removal of words based on percentage.

--- texcur/R/remove.R
+++ texcur/R/remove.R
@@ -11,7 +11,6 @@ rm_regexp <- function(corpus, pattern) {
     .rm_regexp(corpus, pattern)
 }
 
-#' @keywords internal
 .rm_regexp <- function(corpus, pattern) {
     .rm_regexp_helper <- function(text) {
         stringr::str_trim(stringr::str_replace_all(text, pattern, ""))
@@ -73,7 +72,6 @@ rm_whitespace <- function(corpus) {
     .rm_whitespace(corpus)
 }
 
-#' @keywords internal
 .rm_whitespace <- function(corpus) {
     .rm_whitespace_helper <- function(text) {
         removed_spaces <- stringr::str_replace_all(text, "( ){2,}", " ")
@@ -90,8 +88,10 @@ rm_whitespace <- function(corpus) {
 #' @param corpus A dataframe of the corpus
 #' @param tokens A dataframe containing the tokens of the corpus. If not specified, it will be created internally.
 #' @param words A list of words
-#' @param common_word_limit An integer defining the commonity in frequency
-#' @param rare_word_limit An integer defining the rarity in frequency
+#' @param common_word_limit An integer or numeric defining the commonity in frequency
+#' @param rare_word_limit An integer or numeric defining the rarity in frequency
+#' @param percentage If \code{TRUE}, then word limits will be interpreted as percentage, else as counts
+#' @param inclusive If \code{TRUE}, then if the cutoff point is between the same token type all of them will be removed, else none of them will be removed. Only used if \code{percentage}=\code{TRUE}.
 #' @return A filtered corpus
 #'
 #' @export
@@ -100,14 +100,23 @@ rm_words <- function(corpus,
                      words=NULL,
                      common_word_limit=NULL,
                      rare_word_limit=NULL,
+                     percentage=FALSE,
+                     inclusive=FALSE,
                      ...) {
     .check_corpus(corpus, has_id=TRUE)
     .check_tokens(tokens, has_id=TRUE, null_ok=TRUE)
     checkr::assert_character(words, null_ok=TRUE)
-    checkr::assert_integer(common_word_limit, lower=1, null_ok=TRUE)
-    checkr::assert_integer(rare_word_limit, lower=1, null_ok=TRUE)
+    checkr::assert_logical(percentage)
 
-    if (is.null(tokens)) {
+    if (percentage) {
+        checkr::assert_numeric(common_word_limit, len=1, lower=0, upper=1, null_ok=TRUE)
+        checkr::assert_numeric(rare_word_limit, len=1, lower=0, upper=1, null_ok=TRUE)
+    } else {
+        checkr::assert_integer(common_word_limit, len=1, lower=1, null_ok=TRUE)
+        checkr::assert_integer(rare_word_limit, len=1, lower=1, null_ok=TRUE)
+    }
+
+    if (checkr::is_null(tokens)) {
         tokens <- corpus %>% tf_tokenize(...)
     }
 
@@ -118,32 +127,46 @@ rm_words <- function(corpus,
     common_words <- NULL
     rare_words <- NULL
 
-    if (!is.null(common_word_limit)) {
-        common_words <- .get_common_words(token_counts, common_word_limit)
+    if (!checkr::is_null(common_word_limit)) {
+        if (percentage)
+            common_words <- .get_common_words_by_percentage(token_counts, common_word_limit, inclusive)
+        else
+            common_words <- .get_common_words_by_count(token_counts, common_word_limit)
     }
 
-    if (!is.null(rare_word_limit)) {
-        rare_words <- .get_rare_words(token_counts, rare_word_limit)
+    if (!checkr::is_null(rare_word_limit)) {
+        if (percentage)
+            rare_words <- .get_rare_words_by_percentage(token_counts, rare_word_limit, inclusive)
+        else
+            rare_words <- .get_rare_words_by_count(token_counts, rare_word_limit)
     }
 
     words_to_remove <- c(words_to_remove, common_words, rare_words)
 
-    if (is.null(words_to_remove) | length(words_to_remove) == 0) return(corpus)
+    if (checkr::is_null(words_to_remove) | length(words_to_remove) == 0) return(corpus)
 
     .rm_words(corpus, words_to_remove, tokens)
 }
 
-#' @keywords internal
-.get_common_words <- function(token_counts, common_word_limit) {
+.get_common_words_by_count <- function(token_counts, common_word_limit) {
     common_words <- token_counts %>%
         dplyr::filter(n >= common_word_limit) %>%
-        dplyr::mutate(n=NULL)
+        dplyr::select(token)
 
     common_words$token
 }
 
-#' @keywords internal
-.get_rare_words <- function(token_counts, rare_word_limit) {
+.get_common_words_by_percentage <- function(token_counts, common_word_limit, inclusive) {
+    count_limit <- ceiling(sum(token_counts$n) * common_word_limit)
+
+    token_cumsum <- token_counts %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::mutate(n=cumsum(n))
+
+    .get_words_by_percentage(token_cumsum, count_limit, inclusive)
+}
+
+.get_rare_words_by_count <- function(token_counts, rare_word_limit) {
     rare_words <- token_counts %>%
         dplyr::filter(n <= rare_word_limit) %>%
         dplyr::mutate(n=NULL)
@@ -151,7 +174,33 @@ rm_words <- function(corpus,
     rare_words$token
 }
 
-#' @keywords internal
+.get_common_words_by_percentage <- function(token_counts, common_word_limit, inclusive) {
+    count_limit <- ceiling(sum(token_counts$n) * common_word_limit)
+
+    token_cumsum <- token_counts %>%
+        dplyr::arrange(n) %>%
+        dplyr::mutate(n=cumsum(n))
+
+    .get_words_by_percentage(token_cumsum, count_limit, inclusive)
+}
+
+.get_words_by_percentage <- function(token_cumsum, count_limit, inclusive) {
+    token_boundary <- token_cumsum %>%
+        dplyr::mutate(b=n <= count_limit)
+
+    boundary <- which.min(token_boundary$b)
+
+    if (boundary == 1) {
+        if (inclusive) return(token_boundary$token[1])
+        else return(NULL)
+    }
+
+    if (inclusive & token_boundary$n[boundary - 1] / count_limit != 1)
+        return(token_boundary$token[1:boundary])
+
+    token_boundary$token[1:(boundary - 1)]
+}
+
 .rm_words <- function(corpus, words, tokens) {
     words <- dplyr::data_frame(token=words)
 
@@ -159,6 +208,6 @@ rm_words <- function(corpus,
         dplyr::mutate(row=row_number()) %>%
         dplyr::anti_join(words, by="token") %>%
         dplyr::arrange(row) %>%
-        dplyr::mutate(row=NULL) %>%
+        dplyr::mutate(row=NULL)t %>%
         tf_merge_tokens(delim=" ")
 }
