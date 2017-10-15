82f3dab06d0f18afa19b401844e281bef8a1e519
raholm <rasmus.holm723@gmail.com>
2017-10-15 13:36:13 +0200

    Changed word index tokenizer to create matrix rather than dataframe.

--- texcur/src/R_tokenizer.cpp
+++ texcur/src/R_tokenizer.cpp
@@ -5,7 +5,7 @@
 
 using namespace texcur;
 
-Rcpp::DataFrame wit_transform(WordIndexTokenizer* const tokenizer,
+Rcpp::IntegerMatrix wit_transform(WordIndexTokenizer* const tokenizer,
                               const Rcpp::DataFrame& documents) {
   Rcpp::IntegerVector document_ids = documents["id"];
   Rcpp::StringVector texts = documents["text"];
@@ -16,9 +16,10 @@ Rcpp::DataFrame wit_transform(WordIndexTokenizer* const tokenizer,
   for (auto const& token : tokens)
     nrows += token.size();
 
-  Rcpp::IntegerVector res_doc_ids(nrows);
-  Rcpp::IntegerVector res_token_ids(nrows);
-  Rcpp::IntegerVector res_tokens(nrows);
+  // First column is the token id
+  // Second column is the doc id
+  // third column is the token
+  Rcpp::IntegerMatrix result(nrows, 3);
 
   unsigned global_idx = 0;
 
@@ -27,17 +28,15 @@ Rcpp::DataFrame wit_transform(WordIndexTokenizer* const tokenizer,
     auto current_doc_id = document_ids(doc_id);
 
     for (unsigned token_id = 0; token_id < current_tokens.size(); ++token_id) {
-      res_doc_ids(global_idx) = current_doc_id;
-      res_token_ids(global_idx) = token_id + 1;
-      res_tokens(global_idx) = current_tokens.at(token_id);
+      result(global_idx, 1) = token_id + 1;
+      result(global_idx, 2) = current_doc_id;
+      result(global_idx, 3) = current_tokens.at(token_id);
 
       ++global_idx;
     }
   }
 
-  return Rcpp::DataFrame::create(Rcpp::Named("id") = std::move(res_token_ids),
-                                 Rcpp::Named("doc") = std::move(res_doc_ids),
-                                 Rcpp::Named("token") = std::move(res_tokens));
+  return result;
 }
 
 Rcpp::StringVector wit_revert(WordIndexTokenizer* const tokenizer,
